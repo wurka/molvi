6a57ae17cd5a4a7298d56cd12b1e27aa87108e40
Wurka-lenovo <wurka_13@mail.ru>
2019-05-19 11:31:16 +0300

    add work

--- editor/optimizer.py
+++ editor/optimizer.py
@@ -1,5 +1,5 @@
 # оптимизация энергии молекулы
-
+import numpy as np
 
 atom_config = {
 	atoms: [{
@@ -13,7 +13,75 @@ atom_config = {
 
 class NotImplementedException: Exc
 
-def get_bond_energy():
+class Optimizer:
+	mt_table_size = 110
+	p_be_1 = np.zeros((mt_table_size, mt_table_size), dtype=np.float32)
+	p_be_1[5, 5] = 0.318  # C-C
+	p_be_1[5, 0] = -0.454  # C-H
+	p_be_1[0, 5] = -0.454  # H-C 
+	p_be_1[0, 0] = -0.31  # H-H
+
+	d_e = np.zeros((mt_table_size, mt_table_size), dtype=np.float32)
+	d_e[5, 5] = 145.2  # C-C
+	d_e[5, 0] = 183.8  # C-H
+	d_e[0, 5] = 183.8  # H-C 
+	d_e[0, 0] = 168.4  # H-H
+
+	pb0_1 = np.zeros((mt_table_size, mt_table_size), dtype=np.float32)
+	pb0_1[5, 5] = -0.097  # C-C
+	pb0_1[5, 0] = -0.013  # C-H
+	pb0_1[0, 5] = -0.013  # H-C 
+	pb0_1[0, 0] = -0.016  # H-H
+
+	pb0_2 = np.zeros((mt_table_size, mt_table_size), dtype=np.float32)
+	pb0_2[5, 5] = 6.38  # C-C
+	pb0_2[5, 0] = 7.65  # C-H
+	pb0_2[0, 5] = 7.65  # H-C 
+	pb0_2[0, 0] = 5.98  # H-H
+
+	pb0_3 = np.zeros((mt_table_size, mt_table_size), dtype=np.float32)
+	pb0_3[5, 5] = -0.26
+
+	pb0_4 = np.zeros((mt_table_size, mt_table_size), dtype=np.float32)
+	pb0_4[5, 5] = 9.37
+
+	pb0_5 = np.zeros((mt_table_size, mt_table_size), dtype=np.float32)
+	pb0_5[5, 5] = -0.391
+
+	pb0_6 = np.zeros((mt_table_size, mt_table_size), dtype=np.float32)
+	pb0_6[5, 5] = 16.87
+
+	r_0 = np.zeros((mt_table_size), dtype=np.float32)
+	r_0[5] = 1.399
+	r_0[0] = 0.656
+
+	r_0p = np.zeros((mt_table_size), dtype=np.float32)
+	r_0p[5] = 1.266
+
+	r_0pp = np.zeros((mt_table_size), dtype=np.float32)
+	r_0pp[5] = 1.236
+
+	d_e = np.zeros((mt_table_size, mt_table_size), dtype=np.float32)
+	d_e[5, 5] = 145.2
+	d_e[5, 0] = 183.8
+	d_e[0, 5] = 183.8
+	d_e[0, 0] = 168.4
+
+	def bo_(r, i, j):  # r - расстояние между атомами, i, j - индексы элементов в таблице Менделеева
+		if i == j:
+			r0 = r_0[i]
+		else:
+			r0 = 0.5 * (r_0[i] + r_0[j])
+		ans = np.exp(pb0_1[i, j] * (r / r0)**pb0_2[i, j])
+		ans += np.exp(pb0_3[i, j] * (r / r0)**pb0_4[i, j])
+		ans += np.exp(pb0_5[i, j] * (r / r0)**pb0_6[i, j])
+		return ans
+
+	def f1(di, dj, vali, valj):  # vali, valj - валентность атомов i и j соответственно
+		return 0.5*
+
+def get_bond_energy(r, i, j):
+	ans = - d_e[i, j]
 	raise NotImplementedError ("get_bond_energy")
 
 def get_over_energy():
